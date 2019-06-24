72289cdb59a45ca97716985f79134446524b24d3
storytellar <storytellar12@gmail.com>
2019-06-24 23:18:17 +0700

    Not be done, it have some bugs but it's a good idea to solve

--- pacman.js
+++ pacman.js
@@ -1,5 +1,3 @@
-var GameInfo = document.querySelector('.GameInfo');
-
 // 1 = wall
 // 2 = blank space
 // 3 = coin
@@ -8,18 +6,19 @@ var GameInfo = document.querySelector('.GameInfo');
 // 8 = ghost 2
 var map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 2, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 1, 1, 1, 1],
-    [1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 1],
+    [1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+// Object Pacman
 var pacman = {
     // default position (will be updated with init(map) fucntion )
     x: -1,
@@ -43,7 +42,7 @@ var pacman = {
                 }
             }
         }
-        
+
         getAmountOfCoinInMap:
         for (let i = 0; i < map.length; i++) {
             for (let j = 0; j < map[i].length; j++) {
@@ -59,43 +58,43 @@ var pacman = {
             for (let j = 0; j < map[i].length; j++) {
                 const element = map[i][j];
                 if (element === 3) {
-                    this.locationOfcoins.push({x:j,y:i});
+                    this.locationOfcoins.push({ x: j, y: i });
                 }
             }
         }
-        
+
     },
 
     Move: function (Arrow) {
-        var nextPosition;
+        var nextPositionValue;
         switch (Arrow) {
             case "ArrowLeft":
-                nextPosition = map[pacman.y][pacman.x - 1];
-                if (nextPosition === 1) return;
+                nextPositionValue = map[pacman.y][pacman.x - 1];
+                if (nextPositionValue === 1) return;
                 map[pacman.y][pacman.x] = 2;
                 pacman.x = pacman.x - 1;
                 map[pacman.y][pacman.x] = 5;
                 //drawWorld();
                 break;
             case "ArrowUp":
-                nextPosition = map[pacman.y - 1][pacman.x];
-                if (nextPosition === 1) return;
+                nextPositionValue = map[pacman.y - 1][pacman.x];
+                if (nextPositionValue === 1) return;
                 map[pacman.y][pacman.x] = 2;
                 pacman.y = pacman.y - 1;
                 map[pacman.y][pacman.x] = 5;
                 //drawWorld();
                 break;
             case "ArrowRight":
-                nextPosition = map[pacman.y][pacman.x + 1];
-                if (nextPosition === 1) return;
+                nextPositionValue = map[pacman.y][pacman.x + 1];
+                if (nextPositionValue === 1) return;
                 map[pacman.y][pacman.x] = 2;
                 pacman.x = pacman.x + 1;
                 map[pacman.y][pacman.x] = 5;
                 //drawWorld();
                 break;
             case "ArrowDown":
-                nextPosition = map[pacman.y + 1][pacman.x];
-                if (nextPosition === 1) return;
+                nextPositionValue = map[pacman.y + 1][pacman.x];
+                if (nextPositionValue === 1) return;
                 map[pacman.y][pacman.x] = 2;
                 pacman.y = pacman.y + 1;
                 map[pacman.y][pacman.x] = 5;
@@ -104,8 +103,8 @@ var pacman = {
             default:
                 break;
         }
-        
-        switch (nextPosition) {
+
+        switch (nextPositionValue) {
             case 2: // Blank Space => decrease score
                 pacman.EatNothing();
                 break;
@@ -142,13 +141,13 @@ var pacman = {
 pacman.init(map);
 
 
-
+// User press key
 document.onkeydown = function (e) {
     pacman.Move(e.key);
     console.log('\n\n\nĐiểm hiện tại: ' + pacman.score);
     console.log('Vị trí Pacman: ' + pacman.x + '|' + pacman.y);
     pacman.locationOfcoins.forEach(element => {
-        console.log('Coin đang ở vị trí: '+ element.x + '|' + element.y);
+        console.log('Coin đang ở vị trí: ' + element.x + '|' + element.y);
     });
 }
 
@@ -188,3 +187,174 @@ function drawWorld() {
         World.innerHTML += '<br>';
     }
 }
+
+
+
+
+// Tìm theo thuật toán a*
+function FindFastestWay(source, dest, map) {
+    isBlocked = false; // bị chắn bởi tường
+    // Nếu đi thẳng là tới được thì return, thoát đệ quy
+    if (source.x === dest.x) {
+        if (source.y < dest.y) {
+            for (let i = source.y + 1; i < dest.y; i++) {
+                if (map[i][source.x] === 1) {
+                    isBlocked = true;
+                }
+            }
+        }
+        else {
+            for (let i = source.y - 1; dest.y < i; i--) {
+                if (map[i][source.x] === 1) {
+                    isBlocked = true;
+                }
+            }
+        }
+    }
+    if (source.y === dest.y) {
+        if (source.x < dest.x) {
+            for (let i = source.x + 1; i < dest.x; i++) {
+                if (map[source.y][i] === 1) {
+                    isBlocked = true;
+                }
+            }
+        }
+        else{
+            for (let i = source.x ; i> dest.x; i--) {
+                if (map[source.y][i] === 1) {
+                    isBlocked = true;
+                }
+            }
+        }
+    }
+    if (((source.x === dest.x) || (source.y === dest.y)) && isBlocked == false) {
+        return [{ x: source.x, y: source.y }, dest];
+    }
+
+    var nodes = GetNodeNearby(pacman, map);
+    var selectedNode = nodes[0];
+    var f1, f2;
+    for (let i = 1; i < nodes.length; i++) {
+        f1 = GetDistance(selectedNode, dest) + GetDistance(selectedNode, source);
+        f2 = GetDistance(nodes[i], dest) + GetDistance(nodes[i], source);
+        if (f1 > f2) {
+            selectedNode = nodes[i];
+        }
+    }   
+
+    return [].push(FindFastestWay(selectedNode, dest, map));
+}
+
+function GetDistance(node1, node2) {
+    return Math.sqrt(Math.pow((node1.x - node2.x), 2) + Math.pow((node1.y - node2.y), 2));;
+}
+
+// Return positions where pacman can change direction
+// Example [{1,1}, {6,1},...]
+function GetNodeWhereCanChangeDirection(map) {
+    let nodes = [];
+    for (let y = 1; y < map.length - 1; y++) {
+        for (let x = 1; x < map[y].length - 1; x++) {
+            let value = map[y][x];
+            let leftValue = map[y][x - 1];
+            let upValue = map[y - 1][x];
+            let rightValue = map[y][x + 1];
+            let downValue = map[y + 1][x];
+            if ((leftValue !== 1 || rightValue !== 1) && (upValue !== 1 || downValue !== 1) && value !== 1 && map[y][x] !==5)
+                nodes.push({ x: x, y: y });
+        }
+    }
+    console.log(nodes);
+    return nodes;
+}
+
+// Show Node In Map With New Value and draw it
+// Input: Array of Node
+function ShowNodeInMap(nodes, map) {
+    for (let i = 0; i < nodes.length; i++) {
+        node = nodes[i];
+        for (let y = 1; y < map.length - 1; y++) {
+            for (let x = 1; x < map[y].length - 1; x++) {
+                if (y == node.y && x == node.x) {
+                    // new value
+                    map[y][x] = 8;
+                }
+            }
+        }
+    }
+    drawWorld();
+}
+
+
+// Return array of nodes if they're nearby a choosed node
+function GetNodeNearby(node ,map) {
+    let allNodes = GetNodeWhereCanChangeDirection(map);
+    console.log(allNodes);
+    console.log(map);
+    
+    let tempNodes = [];
+    let nodes = [];
+    // thêm tất cả các nodes cùng hàng cùng cột vào tempNodes
+    for (let i = 0; i < allNodes.length; i++) {
+        if ((node.x === allNodes[i].x && node.y !== allNodes[i].y) || (node.x !== allNodes[i].x && node.y === allNodes[i].y))  {
+            tempNodes.push(allNodes[i]);
+        }
+    }
+    
+    
+    // thêm các node bao vây vào nodes
+    for (let i = 0; i < tempNodes.length; i++) {
+        
+        if (node.y === tempNodes[i].y && node.x < tempNodes[i].x) {
+            for (let j = 0; j < tempNodes.length; j++) {
+                if (node.y === tempNodes[j].y && node.x < tempNodes[j].x) {
+                    if (tempNodes[i].x > tempNodes[j].x)
+                        tempNodes[i].x = tempNodes[j].x;
+                }
+
+            }
+            if (map[tempNodes[i].x][tempNodes[i].y] !== 5)
+                nodes.push(tempNodes[i]);
+        }
+        if (node.y === tempNodes[i].y && node.x > tempNodes[i].x) {
+            for (let j = 0; j < tempNodes.length; j++) {
+                if (node.y === tempNodes[j].y && node.x > tempNodes[j].x) {
+                    if (tempNodes[i].x < tempNodes[j].x)
+                        tempNodes[i].x = tempNodes[j].x;
+                }
+
+            }
+            if (map[tempNodes[i].x][tempNodes[i].y] !== 5)
+            nodes.push(tempNodes[i]);
+        }
+        if (node.x === tempNodes[i].x && node.y > tempNodes[i].y) {
+            for (let j = 0; j < tempNodes.length; j++) {
+                if (node.x === tempNodes[i].x && node.y > tempNodes[i].y) {
+                    if (tempNodes[i].y < tempNodes[j].y)
+                        tempNodes[i].y = tempNodes[j].y;
+                }
+
+            }
+            if (map[tempNodes[i].x][tempNodes[i].y] !== 5)
+            nodes.push(tempNodes[i]);
+        }
+        if (node.x === tempNodes[i].x && node.y < tempNodes[i].y) {
+            for (let j = 0; j < tempNodes.length; j++) {
+                if (node.x === tempNodes[i].x && node.y < tempNodes[i].y) {
+                    if (tempNodes[i].y > tempNodes[j].y)
+                        tempNodes[i].y = tempNodes[j].y;
+                }
+
+            }
+            if (map[tempNodes[i].x][tempNodes[i].y] !== 5)
+            nodes.push(tempNodes[i]);
+        }
+    }
+    console.log(nodes);
+    
+    return nodes;
+}
+var nodes = GetNodeNearby(pacman,map);
+// var nodes = GetNodeWhereCanChangeDirection(map);
+ShowNodeInMap(nodes, map);
+// console.log(FindFastestWay(pacman, { x: 11, y: 5 }, map));
